f7933ef5f4d8fb1aa6fe844487660f9f9048b521
a54678861 <a54678861@gmail.com>
2021-01-06 14:28:02 +0800

    test

--- py/001.py
+++ py/001.py
@@ -5,8 +5,8 @@ headers = {
     # 假装自己是瀏覽器
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36',
     # 把Cookie塞進来
-    'Cookie': 'G_ENABLEdvdfvdvd02e37c0629e1602564536',
-    'token': '5efca73dvfdvfdvdfv'
+    'Cookie': 'G_ENABLED_IDPS=google; __cfduid=d58480077fb9db428a9b5602e37c0629e1602564536',
+    'token': '5efca7367aeca35edfaf7ec3.GPJEsW6lUC1625562839594.113955087009517311149'
 }
 conn = sqlite3.connect('C:/Apache24/htdocs/003/sql.db') # ~代表路徑
 c = conn.cursor()
